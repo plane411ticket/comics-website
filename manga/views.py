a0b4491b9458a742dc40dd5b52b24d35b996662c
Ahn <23520027@gm.uit.edu.vn>
2025-03-05 19:17:56 +0700

    delete redundant file

--- manga/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from .models import Manga
-from .serializers import MangaSerializer
-
-class MangaViewSet(viewsets.ModelViewSet):
-    queryset = Manga.objects.all()
-    serializer_class = MangaSerializer
