49997fb5e338ae06a57f0f4e2e4faab8753181a2
plane411ticket <dieuduyen192005@gmail.com>
2025-03-10 23:58:07 +0700

    Track all untracked files

--- frontend/src/screens/Home/HomePage.tsx
+++ frontend/src/screens/Home/HomePage.tsx
@@ -1,11 +1,16 @@
-import { useEffect } from "react";
-export default function HomePage(){
-    useEffect(() => {
-        console.log("HomePage is rendered");
-      }, []);
-    return (
-        <div>
-            <h1>Home Page</h1>
-        </div>
-    );
+import Navbar from "../../components/Navbar";
+
+const HomePage = () => {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Navbar />
+      <div className="container mx-auto text-center py-10">
+        <h2 className="text-3xl font-bold text-gray-800">Welcome to My Manga</h2>
+        <p className="text-gray-600 mt-4">Explore thousands of manga now!</p>
+      </div>
+    </div>
+  );
 };
+
+export default HomePage;
+
