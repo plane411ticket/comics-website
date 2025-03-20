8d0f4dc2e125688d18d7ab3c8ad9be1e09f8822b
plane411ticket <dieuduyen192005@gmail.com>
2025-03-20 20:16:32 +0700

    Add .tsx file

---
+++ frontend/src/screens/Genre/Genre.tsx
@@ -0,0 +1,11 @@
+const Genre = () => {
+   
+
+    return (
+        <div style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
+            <h1 style={{ textAlign: "center", color: "#333" }}>Hello. I have many genres.</h1>
+        </div>
+    );
+};
+
+export default Genre;
