a1bfa815b178a8eaa64458af5ad9dfc18fa24d79
Ahn <23520027@gm.uit.edu.vn>
2025-03-20 16:43:16 +0700

    setting changes

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -211,9 +211,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_CREDENTIALS = True 
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^http://localhost:\d+$",
+    r"^http://127\.0\.0\.1:\d+$",
 ]
 
 CORS_ALLOW_HEADERS = [
