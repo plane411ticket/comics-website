2cb6c6d0806779b1d17964f2ef8acdd5c829ea5d
plane411ticket <dieuduyen192005@gmail.com>
2025-03-22 12:44:25 +0700

    Fix Footer Manga Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./screens//Navbar/Navbar.tsx";
+import Footer from "./screens//Footer/Footer.tsx";
 import "./index.css";
 import useAutoLogin from "./actions/userActions.ts";
 
@@ -8,10 +9,13 @@ const App = () => {
   const location = useLocation();  // Lấy thông tin URL hiện tại
   const hideNavbar = location.pathname === "/auth/login";  // Kiểm tra nếu đang ở trang login
   const hideNavbar02 = location.pathname === "/auth/register";
+  const hideFooter = location.pathname === "/auth/login";  // Kiểm tra nếu đang ở trang login
+  const hideFooter01 = location.pathname === "/auth/register";
   return (
     <div>
       {(!hideNavbar && !hideNavbar02 && <Navbar />)}  {/* Navbar luôn hiển thị */}
       <Outlet />   {/* Nội dung trang sẽ thay đổi ở đây */}
+      {(!hideFooter && !hideFooter01 && <Footer />)}  {/* Navbar luôn hiển thị */}
     </div>
   );
 };
---
+++ frontend/src/screens/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <footer className="bg-black text-white text-center p-3" id="contact">
+            <p>&copy; 2025 Cabbage🥬 Monkey🙊 Unicorn🦄. All rights reserved.</p>
+            <div>
+                <a className="text-white me-3">Chính sách bảo mật</a>
+                <a className="text-white me-3">Điều khoản dịch vụ</a>
+                <a className="text-white me-3">Liên hệ hỗ trợ</a>           
+            </div>
+        </footer>
+    )}
+export default Footer
