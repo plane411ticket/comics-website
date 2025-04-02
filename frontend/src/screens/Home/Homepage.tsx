aac277dfc5429415cca49734485ecaf33191499b
Kh-ai10 <23520673@gm.uit.edu.vn>
2025-04-02 22:32:05 +0700

    h1

---
+++ frontend/src/screens/Home/Homepage.tsx
@@ -0,0 +1,10 @@
+//import Navbar from "../../screens/Navbar/Navbar.tsx"; // Import đúng file Navbar
+/* 
+    <div className="min-h-screen bg-white " id="Homepage" >
+      <div className="container mx-auto text-center py-10">
+        <h2 className="text-3xl font-bold text-gray-800">Welcome to My Manga</h2>
+        <p className="text-gray-600 mt-4">Explore thousands of manga now!</p>s
+      </div>
+    </div> 
+
+ */
