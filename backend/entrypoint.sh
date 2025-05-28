e71defc5c47659f84282357daba8ff5d6eb93036
plane411ticket <dieuduyen192005@gmail.com>
2025-05-28 20:53:03 +0700

    Add script to create superuser

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,5 +3,8 @@ python manage.py makemigrations
 echo "🔧 Chạy migrate..."
 python manage.py migrate
 
+echo ">> Creating superuser..."
+python3 create_superuser.py
+
 echo "🚀 Khởi động server..."
 python manage.py runserver 0.0.0.0:8000
