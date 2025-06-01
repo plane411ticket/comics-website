4c62093d678d15e2e762c1cecf4193396b4403e4
plane411ticket <dieuduyen192005@gmail.com>
2025-06-01 19:11:15 +0700

    Import genres, Mangas, Novels

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,6 +3,7 @@
 python manage.py makemigrations
 echo "🔧 Chạy migrate..."
 python manage.py migrate
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', '@Admin0123456') if not User.objects.filter(username='admin').exists() else print('Superuser exists')" | python3 manage.py shell
 
 echo "🚀 Khởi động server..."
 python manage.py runserver 0.0.0.0:8000
