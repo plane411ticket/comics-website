daa7711ae0f4392dc14a03c6f785c0c373bbb21f
plane411ticket <dieuduyen192005@gmail.com>
2025-06-01 21:31:45 +0700

    Import genres, Mangas, Novels

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,11 +6,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
+  // resolve: {
+  //   alias: {
+  //     "@": "/src",
+  //   },
+  // },
   server: {
     proxy: {
       '/api': {
