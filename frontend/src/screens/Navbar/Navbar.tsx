2b78a7e284aa81af4b67b87aa2ee02d961be0ae1
Ahn <23520027@gm.uit.edu.vn>
2025-06-02 10:13:20 +0700

    fix

--- frontend/src/screens/Navbar/Navbar.tsx
+++ frontend/src/screens/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useState, useEffect} from "react";
+import { useState} from "react";
 import { navbarItems } from "../../components/Navbarapi";
 import { FiMenu } from "react-icons/fi";
 import { useSelector } from "react-redux";
